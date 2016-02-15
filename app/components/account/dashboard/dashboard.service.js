45103cdff1fb5e533fef45ae7f31ceb53a9961e8
Kishore Ganigapeta <kishore.ganigapenta@mtuity.com>
2016-02-15 20:36:24 +0530

    Sonar/junkins issue fixed

--- app/components/account/dashboard/dashboard.service.js
+++ app/components/account/dashboard/dashboard.service.js
@@ -5,7 +5,6 @@ angular.module('app').factory('DashboardService', ['$http', '$rootScope', functi
   var service = {};
   var base_url = "http://ec2-54-159-195-71.compute-1.amazonaws.com";
   var userID = $rootScope.globals.currentUser.id;
-  var fd = new FormData();
 
   service.getAllApi = function () {
     return $http.get(base_url + '/newsfeeds/' + userID + '/listnewsfeeds');
@@ -16,7 +15,6 @@ angular.module('app').factory('DashboardService', ['$http', '$rootScope', functi
   };
 
   return service;
-
 
 }]);
 
