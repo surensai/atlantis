131c0842ad08c5dc2c22a3dbd7bb0ee1cdc813e4
Kishore Ganigapeta <kishore.ganigapenta@mtuity.com>
2016-02-18 11:42:49 +0530

    Google plus share implementation

--- app/common/app-directives/social-share/twitter.directive.js
+++ app/common/app-directives/social-share/twitter.directive.js
@@ -1,13 +1,17 @@
 'use strict';
-angular.module("app").directive('twitterShare', ['$timeout', function ($timeout) {
+angular.module("app").directive('twitterShare', ['$window', function ($window) {
   return {
     restrict: 'A',
     scope: {
       index: '=index',
       item: '=itemdata'
     },
-    template: '<a class="fa fa-twitter fa-lg" href="http://twitter.com/share?url={{item.files}}&text={{item.title}}" target="_blank"></a>',
-    link: function () {
+    template: '<a class="fa fa-twitter fa-lg"></a>',
+    link: function (scope, element) {
+      element.bind('click', function () {
+        $window.open("https://twitter.com/share?url=" + scope.item.files + "&text=" + scope.item.title, "", "width=500,height=260,left=200,top=150,menubar=0");
+        return false;
+      });
     }
   };
 }]);
