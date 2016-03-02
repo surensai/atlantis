1e80a9685339b0b6b778810922af42e9fdbec22b
Sriram-27727 <kiran.potharaju@mtuity.com>
2016-03-02 14:46:31 +0530

    Navigate Login and display message after change and reset password issue fixed

---
+++ app/common/app-directives/valid-file.directive.js
@@ -0,0 +1,13 @@
+angular.module("app").directive('validFile',function(){
+    return {
+        require:'fileModel',
+        link:function(scope,el,attrs,ngModel){
+            el.bind('change',function(){
+                scope.$apply(function(){
+                    ngModel.$setViewValue(el.val());
+                    ngModel.$render();
+                });
+            });
+        }
+    }
+});
