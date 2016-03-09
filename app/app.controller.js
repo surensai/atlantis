4ff1be4dfca31107b3d638a47a867d7c36f25e4b
Kishore Ganigapeta <kishore.ganigapenta@mtuity.com>
2016-03-09 18:08:37 +0530

    Carey comments

--- app/app.controller.js
+++ app/app.controller.js
@@ -6,16 +6,6 @@ angular.module("app").controller("appCtrl", ['$scope', '$rootScope', '$location'
     $state.go('login');
   };
 
-  $scope.headerLogoNavigation = function () {
-    var loggedIn = $rootScope.globals.currentUser;
-    if (!loggedIn) {
-      $state.go('login');
-    }
-    else {
-      $state.go('account.dashboard');
-    }
-  };
-
   $rootScope.$on('$locationChangeStart', function () {
     $scope.curUrl =$location.path();
   });
