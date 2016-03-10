d7d97d4784102723c53d1dfb3fd733195f429f0b
Kishore Ganigapeta <kishore.ganigapenta@mtuity.com>
2016-03-10 13:03:23 +0530

    Http Interceptors

--- app/app.config.js
+++ app/app.config.js
@@ -1,5 +1,5 @@
 'use strict';
-var app = angular.module('app').config(['$windowProvider', '$translateProvider','$httpProvider', function ($windowProvider, $translateProvider, $httpProvider) {
+var app = angular.module('app').config(['$windowProvider', '$translateProvider', '$httpProvider', function ($windowProvider, $translateProvider, $httpProvider) {
 
   $translateProvider.useStaticFilesLoader({
     prefix: 'assets/i18n/',
@@ -16,22 +16,41 @@ var app = angular.module('app').config(['$windowProvider', '$translateProvider',
   $translateProvider.preferredLanguage('en');
   $translateProvider.useSanitizeValueStrategy('escape');
 
-  $httpProvider.interceptors.push(['$q', '$location', '$localStorage', function ($q, $location, $localStorage) {
-        return {
-            'request': function (config) {
-                config.headers = config.headers || {};
-                if ($localStorage.token) {
-                    config.headers.Authorization = 'Bearer ' + $localStorage.token;
-                }
-                return config;
-            },
-            'responseError': function (response) {
-                if (response.status === 401 || response.status === 403) {
-                    $location.path('/login');
-                }
-                return $q.reject(response);
-            }
-        };
+  $httpProvider.interceptors.push(['$q', '$location', '$localStorage', '$rootScope', function ($q, $location, $localStorage, $rootScope) {
+    $rootScope.ajaxProgress = 0;
+
+    if (!String.prototype.contains) {
+      String.prototype.contains = function (str) {
+        return (this.indexOf(str) !== -1);
+      };
+    }
+
+    return {
+      'request': function (config) {
+        var str = config.url;
+        config.headers = config.headers || {};
+        if ($localStorage.token) {
+          config.headers.Authorization = 'Bearer ' + $localStorage.token;
+        }
+
+        $rootScope.ajaxProgress++;
+        return config;
+      },
+      'response': function (response) {
+        var str = response.config.url;
+        $rootScope.ajaxProgress--;
+        return response;
+      },
+      'responseError': function (rejection) {
+        var str = rejection.config.url;
+        if (rejection.status === 401 || rejection.status === 403) {
+          $location.path('/login');
+        }
+        $rootScope.ajaxProgress--;
+        return $q.reject(response);
+      }
+    };
+
   }]);
 
 }]);
