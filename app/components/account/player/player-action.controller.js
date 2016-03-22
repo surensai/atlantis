8a05ad164d1bc112dd6e68e093ec425cf3f6c947
bnvkishore <kishoreb.n.v209@gmail.com>
2016-03-22 12:10:27 +0530

    image croping implemented

--- app/components/account/player/player-action.controller.js
+++ app/components/account/player/player-action.controller.js
@@ -86,7 +86,7 @@ angular.module("app").controller('playerActionCtrl', ['$scope', '$state', 'flash
 
   function uploadProfilePic(form) {
     var handleSuccess = function (data) {
-      playerAction.model.playerItem.profileURL = data.files[0].url;
+      playerAction.model.playerItem.profileURL = playerAction.model.myCroppedImage;
       if (playerAction.isUpdate) {
         updateAction();
       } else {
@@ -134,7 +134,7 @@ angular.module("app").controller('playerActionCtrl', ['$scope', '$state', 'flash
 
 
   playerAction.fileReaderSupported = window.FileReader != null;
-
+  playerAction.model.myCroppedImage = '';
   $scope.photoChanged = function (files) {
     if (files != null) {
       var file = files[0];
